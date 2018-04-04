8010dba16aefa7e29951adb3257ea83a450c86d1
Jonathan Rukundo <talk2rukundo@gmail.com>
2018-04-04 10:30:53 +0300

    Initial commit

---
+++ app/src/main/java/com/birungi/maureen/phonetracker/GpsToggler.java
@@ -0,0 +1,42 @@
+package com.birungi.maureen.phonetracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+/**
+ * GPS enabling exploit according to http://stackoverflow.com/questions/4721449/how-can-i-enable-or-disable-the-gps-programmatically-on-android
+ */
+public class GpsToggler {
+
+    public static void enableGps(Context context) {
+        String provider = Settings.Secure.getString(context.getContentResolver(),
+                Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if (!provider.contains("gps")) { // if gps is disabled
+            final Intent poke = new Intent();
+
+            poke.setClassName("com.android.settings",
+                    "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            context. sendBroadcast(poke);
+        }
+    }
+
+    public static void disableGps(Context context) {
+        String provider = Settings.Secure.getString(context.getContentResolver(),
+                Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if (provider.contains("gps")) { // if gps is enabled
+            final Intent poke = new Intent();
+
+            poke.setClassName("com.android.settings",
+                    "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            context.sendBroadcast(poke);
+        }
+    }
+}
---
+++ app/src/main/java/com/birungi/maureen/phonetracker/LogAlarmReceiver.java
@@ -0,0 +1,71 @@
+package com.birungi.maureen.phonetracker;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
+public class LogAlarmReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_TRIGGER_ALARM = "at.fhj.ase13.mobcomp2.phonetracker.TRIGGER_ALARM";
+
+    private static final String EXTRA_PHONE_NUMBER = "PHONE_NUMBER";
+
+    private static PendingIntent pendingTrigger;
+
+    private static int ALARM_TIME = 60 * 5 * 1000;
+
+    public static synchronized void startAlarm(Context context, String phoneNumber) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_TRIGGER_ALARM);
+        intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber);
+
+        context.sendBroadcast(intent);
+        enqueueAlarm(context, phoneNumber);
+    }
+
+    public static synchronized void enqueueAlarm(Context context, String phoneNumber) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_TRIGGER_ALARM);
+        intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber);
+
+        pendingTrigger = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + ALARM_TIME, pendingTrigger);
+    }
+
+    public static synchronized void stopAlarm(Context context) {
+        if (pendingTrigger == null) {
+            return;
+        }
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingTrigger);
+
+        pendingTrigger.cancel();
+        pendingTrigger = null;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent == null || !ACTION_TRIGGER_ALARM.equals(intent.getAction())) {
+            return;
+        }
+
+        if (intent.getStringExtra(EXTRA_PHONE_NUMBER) == null
+                || intent.getStringExtra(EXTRA_PHONE_NUMBER).length() == 0) {
+            return;
+        }
+
+        SmsLocationReporter sender = new SmsLocationReporter(context,
+                intent.getStringExtra(EXTRA_PHONE_NUMBER));
+        sender.report();
+
+        // reschedule the alarm after the SMS has been sent
+        enqueueAlarm(context, intent.getStringExtra(EXTRA_PHONE_NUMBER));
+    }
+}
